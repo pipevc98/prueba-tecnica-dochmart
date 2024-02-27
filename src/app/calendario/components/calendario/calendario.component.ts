c061fdfaba9f3aba3979244444538dc31de0201b
Felipe Ceballos <dejesus.valdovinos@alumnos.udg.mx>
2024-02-27 11:41:02 -0600

    final 1.1

--- src/app/calendario/components/calendario/calendario.component.ts
+++ src/app/calendario/components/calendario/calendario.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewChecked, AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { CalendarioService } from '../../services/calendario-service.service';
 
@@ -6,7 +6,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { DiaComponentComponent } from '../dia-component/dia-component.component';
 import { infoComponent } from '../info/info.component';
 
-import { CalendarOptions, DayCellContentArg } from '@fullcalendar/core';
+import { CalendarOptions } from '@fullcalendar/core';
 import { FullCalendarComponent } from '@fullcalendar/angular';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
@@ -23,26 +23,17 @@ import bootstrap5Plugin from '@fullcalendar/bootstrap5'
   templateUrl: './calendario.component.html',
   styles: []
 })
-export class CalendarioComponent implements OnInit, AfterViewChecked{
+export class CalendarioComponent implements OnInit{
 
   public reservaciones: any[] = []
 
   @ViewChild('calendar') calendarComponent!: FullCalendarComponent;
-
   constructor( private calendarioService: CalendarioService, private modalService: NgbModal, ) {
   }
-
   //Inicializar los datos de la api
   ngOnInit(): void {
     this.getReservaciones();  
   }
-
-  ngAfterViewChecked(): void {
-    // this.calendarOptions.dayCellDidMount = (arg) =>{ this.fechasDisplonibles(arg) }
-  }
-
-
-
   //funcion para hacer el get a la api
   getReservaciones () {
     this.calendarioService.getReservaciones().subscribe( ( reservaciones: any ) => {
@@ -57,10 +48,8 @@ export class CalendarioComponent implements OnInit, AfterViewChecked{
         // display: 'background'
       }));
       this.calendarOptions.events = events
-      
     });
   }
-
   //Abrir fromulario para agregar nueva reserva
   openFormReserva(info: any) {
 		const modalRef = this.modalService.open(DiaComponentComponent);
@@ -79,27 +68,24 @@ export class CalendarioComponent implements OnInit, AfterViewChecked{
   // Cambiar de color segun si hay fechas disponibles o no
   fechasDisplonibles( info: any = [] ) {
     
-    const fecha = info.date
+    const fecha = info.date;
 
     const eventosDelDia = this.calendarComponent.getApi().getEvents().filter( (evento: any) => {
-    
       return evento.start!.toDateString() === fecha.toDateString();
-      
     });
     
-
-    const cell = info.el
+    console.log(eventosDelDia)
+    console.log(info)
 
     if(eventosDelDia.length > 11){
-      cell.style.backgroundColor = '#F78181'
+      return { html: `<div class="bg-danger fondo my-class d-flex justify-content-end"><p class="text-white" >${info.dayNumberText}</p></div>`}
+      
     }else {
-      cell.style.backgroundColor =  '#CEF6CE'
+      return { html: `<div class="bg-success fondo my-class d-flex justify-content-end"><p class="text-white" >${info.dayNumberText}</p></div>`}
     }
 
+
   }
-
-  
-
   //Configuaracion del calendario
   calendarOptions: CalendarOptions = ({
     plugins: [
@@ -112,16 +98,13 @@ export class CalendarioComponent implements OnInit, AfterViewChecked{
     ],
     themeSystem: 'bootstrap5',
     initialView: 'dayGridMonth',
-
     initialDate: "2024-09-01",
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
       right: 'dayGridMonth,timeGridDay,listWeek'
     },
-    
     weekends: true,
-    dayMaxEvents: 0,
     eventClick: this.onShowReserva.bind(this),
     navLinks: true,
     editable: true,
@@ -130,27 +113,26 @@ export class CalendarioComponent implements OnInit, AfterViewChecked{
     slotLabelFormat: { hour: 'numeric', minute: '2-digit', omitZeroMinute: false, meridiem: 'short' },
     eventBackgroundColor: '#F78181', 
     eventBorderColor: '#F78181',
-    businessHours: { startTime: '07:00', endTime: '18:00', daysOfWeek:[ 0, 1, 2, 3, 4, 5, 6 ]},
+    eventTextColor: '#FFFFFF',
+    businessHours: { startTime: '07:00', endTime: '19:00', daysOfWeek:[ 0, 1, 2, 3, 4, 5, 6 ]},
     slotMinTime: '07:00:00',
     slotMaxTime: '19:00:00',
     allDaySlot: false,
-    eventMaxStack: 1,
-    dayCellDidMount: (arg) => this.fechasDisplonibles(arg),
+    eventOverlap: true,
     views: {
       timeGridDay: {
         plugins: [interactionPlugin, timeGridPlugin],
         dateClick: this.openFormReserva.bind(this),
         eventClick: this.onShowReserva.bind(this),
-        selectOverlap: false,
         contentHeight:328,
-        
+        eventDisplay: 'background',
       },
       dayGridMonth: {
+        dayMaxEvents: 0,
+        dayMaxEventRows: 1,
         dateClick: this.onShowDay.bind(this),
-        defaultAllDayEventDuration: 12
+        dayCellContent: (arg) => this.fechasDisplonibles(arg),
       },
-
     },
   });
-  
 }
